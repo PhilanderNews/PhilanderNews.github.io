a17981b87fcd73c96334e50d613d56518dd22a8b
Befous <befousmain@gmail.com>
2023-11-12 04:40:41 +0700

    Add cookie.js : export function setCookieWithExpireDay, setCookieWithExpireHour, setCookieWithExpireSecond dan deleteCookie

---
+++ theme/js/cookie.js
@@ -0,0 +1,25 @@
+export function setCookieWithExpireDay(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function setCookieWithExpireHour(cname, cvalue, exhour) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exhour * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function setCookieWithExpireSecond(cname, cvalue, exsecs) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exsecs * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function deleteCookie(cname) {
+  document.cookie = cname + "= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
