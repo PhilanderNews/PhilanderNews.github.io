397b5fc423c33cb5a2d7695edb3561d582121f73
Befous <befousmain@gmail.com>
2023-11-18 02:03:19 +0700

    Update post.js : ubah nama function

--- source/js/post.js
+++ source/js/post.js
@@ -2,10 +2,10 @@ import LoginUser from "./postLoginUser.js";
 import RegistrasiUser from "./postRegistrasiUser.js";
 import Logout from "./postLogout.js";
 import HapusCookie from "./postDeleteCookie.js";
-import checkCookie from "./postCheckCookie.js";
+import checkCookieUser from "./postCheckCookieUser.js";
 
 window.LoginUser = LoginUser;
 window.RegistrasiUser = RegistrasiUser;
 window.Logout = Logout;
 window.HapusCookie = HapusCookie;
-window.checkCookie = checkCookie;
+window.checkCookieUser = checkCookieUser;
