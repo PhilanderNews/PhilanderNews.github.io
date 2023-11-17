b5ece97411b93e2d6887a5d6ceb94c668b2fd47a
Befous <befousmain@gmail.com>
2023-11-17 19:27:32 +0700

    Update postRegistrasiUser.js : hapus setCookie

--- theme/js/postRegistrasiUser.js
+++ theme/js/postRegistrasiUser.js
@@ -1,8 +1,7 @@
-import { postNoToken } from "./api.js";
-import {setInner,getValue} from "./element.js";
-import {setCookieWithExpireHour} from "./cookie.js";
+import { postBiasa } from "./api.js";
+import { setInner,getValue } from "./element.js";
 
-export default function Login(){
+export default function RegistrasiUser(){
     let target_url = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-RegistrasiUser";
     let datainjson = {
         "name": getValue("name"),
@@ -10,14 +9,11 @@ export default function Login(){
         "username": getValue("username"),
         "password": getValue("password")
     }
-
-    postNoToken(target_url,datainjson,responseData);
-
+    postBiasa(target_url,datainjson,responseData);
 }
 
 function responseData(result) {
     setInner("pesan", result.message);
-    setCookieWithExpireHour("token", result.token, 2);
     if (result.message == "Username telah dipakai") {
         alert("Username telah dipakai");
     }
