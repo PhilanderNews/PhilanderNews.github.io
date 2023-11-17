c96c2707d18696d70b9c634698cb5c731dade68f
Befous <befousmain@gmail.com>
2023-11-17 19:16:04 +0700

    Update post.js : import function Logout HapusCookie checkCookie

--- theme/js/post.js
+++ theme/js/post.js
@@ -1,5 +1,11 @@
 import LoginUser from "./postLoginUser.js";
 import RegistrasiUser from "./postRegistrasiUser.js";
+import Logout from "./postLogout.js";
+import HapusCookie from "./postDeleteCookie.js";
+import checkCookie from "./postCheckCookie.js";
 
 window.LoginUser = LoginUser;
 window.RegistrasiUser = RegistrasiUser;
+window.Logout = Logout;
+window.HapusCookie = HapusCookie;
+window.checkCookie = checkCookie;
