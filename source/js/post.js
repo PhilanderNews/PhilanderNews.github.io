87ea86ec3cbbdd2a1e7dc71b3945d600a0e4cd5e
Befous <befousmain@gmail.com>
2023-11-18 02:27:18 +0700

    Update post.js : tambah function checkCookieAdmin

--- source/js/post.js
+++ source/js/post.js
@@ -3,9 +3,11 @@ import RegistrasiUser from "./postRegistrasiUser.js";
 import Logout from "./postLogout.js";
 import HapusCookie from "./postDeleteCookie.js";
 import checkCookieUser from "./postCheckCookieUser.js";
+import checkCookieAdmin from "./checkCookieAdmin.js";
 
 window.LoginUser = LoginUser;
 window.RegistrasiUser = RegistrasiUser;
 window.Logout = Logout;
 window.HapusCookie = HapusCookie;
 window.checkCookieUser = checkCookieUser;
+window.checkCookieAdmin = checkCookieAdmin;
