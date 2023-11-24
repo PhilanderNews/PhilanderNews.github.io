1f24e2dd919eb3dbacc1da219aec3226f46c7ce8
Befous <befousmain@gmail.com>
2023-11-24 07:35:49 +0700

    Delete : ga kepakek

--- assets/js/postDeleteCookie.js
+++
@@ -1,8 +0,0 @@
-import { deleteCookie } from "./cookie.js";
-
-export default function HapusCookie() {
-    deleteCookie();
-    sessionStorage.clear("tokenuser")
-    sessionStorage.clear("tokenadmin")
-    sessionStorage.clear("name")
-}
