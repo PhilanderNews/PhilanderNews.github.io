972747f939b4005703c76beacaf49a273a4171f6
Befous <befousmain@gmail.com>
2023-11-24 07:38:59 +0700

    Update : menyesuaikan result sama response api

--- assets/js/postLoginUser.js
+++ assets/js/postLoginUser.js
@@ -1,7 +1,7 @@
 import { postBiasa } from "./api.js";
 import { getValue } from "./element.js";
-import { setCookieWithExpireHour } from "./cookie.js";
-import { APILogin, userrahasia, adminrahasia } from "./rahasia.js"
+import { setCookieWithExpireSecond } from "./cookie.js";
+import { APILogin } from "./rahasia.js"
 
 const loadingIndicator = document.getElementById("loadingIndicator");
 
@@ -31,16 +31,12 @@ function responseData(result) {
     if (result.message == "Password Salah") {
         alert("Password Salah");
     }
-    if (result.role == "user") {
-        setCookieWithExpireHour("tokenuser", result.token, 2);
-        sessionStorage.setItem("name", result.name)
-        sessionStorage.setItem("role", userrahasia)
-        window.location.href = `../${result.role}/beranda`;
+    if (result.data.role.admin == false) {
+        alert("Anda tidak terdaftar sebagai admin");
     }
-    if (result.role == "admin") {
-        setCookieWithExpireHour("tokenadmin", result.token, 2);
-        sessionStorage.setItem("name", result.name)
-        sessionStorage.setItem("role", adminrahasia)
-        window.location.href = `../${result.role}/beranda`;
+    if (result.data.role.author == false) {
+        alert("Anda tidak terdaftar sebagai author");
     }
+    setCookieWithExpireSecond("token", result.token, 1);
+    window.location.href = "../user/beranda";
 }
