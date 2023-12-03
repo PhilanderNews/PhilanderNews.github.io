33e49db96799c778b6fc58218791ad17d6d5d254
Befous <befousmain@gmail.com>
2023-12-03 21:57:53 +0700

    Update : tambah link api

--- assets/js/dll/rahasia.js
+++ assets/js/dll/rahasia.js
@@ -1,5 +1,10 @@
+// user
 export let APIRegistrasi = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Registrasi";
 export let APILogin = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Login";
 export let APIAuthorization = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Authorization";
+// berita
 export let APIAmbilDataBerita = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-AmbilSemuaBerita";
-export let APITambahBerita = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-TambahBerita-Test";
+export let APITambahBerita = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-TambahBerita";
+// komentar
+export let APIAmbilDataKomentar = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-AmbilSemuaKomentar";
+export let APITambahKomentar = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-TambahKomentar";
