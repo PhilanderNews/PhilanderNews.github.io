6df998a785ea72234fb5d1dfd8c94019432988bb
Befous <befousmain@gmail.com>
2023-12-03 10:28:37 +0700

    Update : pindah folder

--- assets/js/rahasia.js
+++
@@ -1,5 +0,0 @@
-export let APIRegistrasi = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Registrasi";
-export let APILogin = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Login";
-export let APIAuthorization = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-Authorization";
-export let APIAmbilDataBerita = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-AmbilDataBerita-Test";
-export let APITambahBerita = "https://asia-southeast2-befous.cloudfunctions.net/PhilanderNews-TambahBerita-Test";
