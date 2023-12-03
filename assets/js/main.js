1b5aa758ebce3bcc3b628431ef7a4e3e34c32a83
Befous <befousmain@gmail.com>
2023-12-03 21:56:27 +0700

    Update : import function SemuaKomentarSingleBlog, TambahKomentar

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,41 @@ import Logout from "./dll/postLogout.js";
 import SemuaBeritaBeranda from "./berita/SemuaBeritaBeranda.js"
 import SemuaBeritaSingleBlog from "./berita/SemuaBeritaSingleBlog.js";
 import Authorization from "./user/Authorization.js";
+import SemuaKomentarSingleBlog from "./komentar/SemuaKomentarSingleBlog.js";
+import TambahKomentar from "./komentar/TambahKomentar.js";
 
 window.Login = Login;
 window.RegistrasiUser = RegistrasiUser;
 window.Logout = Logout;
+window.TambahKomentar = TambahKomentar;
 
 // Check if the element with ID 'preview-content' exists before calling getSemuaBerita
 const previewContentElement = document.getElementById('preview-content');
 const auth = document.getElementById('need-authorization');
+const fullberitaElement = document.getElementById('full-berita');
+const komentarberitaElement = document.getElementById('komentar-berita');
+var divlogout = document.getElementById("logout");
+var divcms = document.getElementById("cms");
+var divlogin = document.getElementById("login");
+
+
 if (previewContentElement) {
     SemuaBeritaBeranda();
 } else {
-    console.log("Element with ID 'preview-content' not found. getSemuaBerita not executed.");
+    console.log("Element with ID 'preview-content' not found. SemuaBeritaBerita not executed.");
 }
 
-const fullberitaElement = document.getElementById('full-berita');
+
 if (fullberitaElement) {
     SemuaBeritaSingleBlog();
 } else {
-    console.log("Element with ID 'full-berita' not found. SemuaBeritaSingleBloga not executed.");
+    console.log("Element with ID 'full-berita' not found. SemuaBeritaSingleBlog not executed.");
+}
+
+if (komentarberitaElement) {
+    SemuaKomentarSingleBlog();
+} else {
+    console.log("Element with ID 'komentar-berita' not found. SemuaKomentarSingleBlog not executed.");
 }
 
 if (auth) {
@@ -34,22 +50,19 @@ if (auth) {
 // Cek apakah cookie ada
 if (!document.cookie.includes("token")) {
     // Jika tidak ada, sembunyikan div dengan ID tertentu
-    var divToHide = document.getElementById("logout");
-    if (divToHide) {
-      divToHide.style.display = "none";
+    if (divlogout) {
+        divlogout.style.display = "none";
     } else {
         console.log("Element with ID 'logout' not found.");
     }
-    var divToHide2 = document.getElementById("cms");
-    if (divToHide2) {
-      divToHide2.style.display = "none";
+    if (divcms) {
+        divcms.style.display = "none";
     } else {
-        console.log("Element with ID 'logout' not found.");
+        console.log("Element with ID 'cms' not found.");
     }
 } else {
-    var divToHide = document.getElementById("login");
-    if (divToHide) {
-      divToHide.style.display = "none";
+    if (divlogin) {
+        divlogin.style.display = "none";
     } else {
         console.log("Element with ID 'login' not found.");
     }
