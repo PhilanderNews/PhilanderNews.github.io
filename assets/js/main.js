3c720a1458ab708883ed4028c582857458f3090f
Befous <befousmain@gmail.com>
2023-12-04 17:14:11 +0700

    Update : tambah function yang dipakek

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,49 @@ import Logout from "./dll/Logout.js";
 import Login from "./user/Login.js";
 import RegistrasiUser from "./user/RegistrasiUser.js";
 import Authorization from "./user/Authorization.js";
+import HapusUser from "./user/DeleteSatuUser.js";
+import GetSemuaUserCMS from "./user/GetSemuaUserCMS.js";
+import PostSatuUserCMS from "./user/PostSatuUserCMS.js";
+import TambahUser from "./user/TambahUser.js";
+import UpdateUser from "./user/UpdateUser.js";
 // Berita
 import PostSatuBerita from "./berita/PostSatuBerita.js";
 import GetSemuaBeritaBeranda from "./berita/GetSemuaBeritaBeranda.js";
+import GetSemuaBeritaCMS from "./berita/GetSemuaBeritaCMS.js";
+import TambahBerita from "./berita/TambahBerita.js";
+import UpdateBerita from "./berita/UpdateBerita.js";
+import PostSatuBeritaCMS from "./berita/PostSatuBeritaCMS.js";
+import HapusBerita from "./berita/DeleteSatuBerita.js";
 // Komentar
 import GetSemuaKomentarBlog from "./komentar/GetSemuaKomentarBlog.js";
 import TambahKomentar from "./komentar/TambahKomentar.js";
+import HapusKomentar from "./komentar/DeleteSatuKomentar.js";
+import GetSemuaKomentarCMS from "./komentar/GetSemuaKomentarCMS.js";
+import PostSatuKomentarCMS from "./komentar/PostSatuKomentarCMS.js";
 
+// Tools
+window.Logout = Logout;
+// User
 window.Login = Login;
 window.RegistrasiUser = RegistrasiUser;
-window.Logout = Logout;
+window.HapusUser = HapusUser;
+window.PostSatuUserCMS = PostSatuUserCMS;
+window.TambahUser = TambahUser;
+window.UpdateUser = UpdateUser;
+// Berita
+window.TambahBerita = TambahBerita;
+window.UpdateBerita = UpdateBerita;
+window.HapusBerita = HapusBerita;
+window.PostSatuBeritaCMS = PostSatuBeritaCMS;
+// Komentar
+window.HapusKomentar = HapusKomentar;
 window.TambahKomentar = TambahKomentar;
+window.PostSatuKomentarCMS = PostSatuKomentarCMS;
 
-const getallElement = document.getElementById('semua-berita');
+const getallBeritaElement = document.getElementById('semua-berita');
+const getallBeritaCMSElement = document.getElementById('semua-berita-cms');
+const getallKomentarCMSElement = document.getElementById('semua-komentar-cms');
+const getallUserCMSElement = document.getElementById('semua-user-cms');
 const auth = document.getElementById('need-authorization');
 const getsatuberita = document.getElementById('satu-berita');
 const komentarberitaElement = document.getElementById('komentar-berita');
@@ -24,10 +54,28 @@ const divlogout = document.getElementById("logout");
 const divcms = document.getElementById("cms");
 const divlogin = document.getElementById("login");
 
-if (getallElement) {
+if (getallBeritaElement) {
     GetSemuaBeritaBeranda();
 } else {
     console.log("Element with ID 'semua-berita' not found. GetSemuaBeritaBeranda not executed.");
+}
+
+if (getallBeritaCMSElement) {
+    GetSemuaBeritaCMS();
+} else {
+    console.log("Element with ID 'semua-berita-cms' not found. GetSemuaBeritaCMS not executed.");
+}
+
+if (getallKomentarCMSElement) {
+    GetSemuaKomentarCMS();
+} else {
+    console.log("Element with ID 'semua-komentar-cms' not found. GetSemuaKomentarCMS not executed.");
+}
+
+if (getallUserCMSElement) {
+    GetSemuaUserCMS();
+} else {
+    console.log("Element with ID 'semua-user-cms' not found. GetSemuaUserCMS not executed.");
 }
 
 if (getsatuberita) {
