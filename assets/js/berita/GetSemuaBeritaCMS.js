05881ebb6e8a10a585aba94787b0dbffdb069cbd
Ibrohim Mubarok <befousmain@gmail.com>
2024-06-24 22:41:18 +0700

    Benerin update berita

--- assets/js/berita/GetSemuaBeritaCMS.js
+++ assets/js/berita/GetSemuaBeritaCMS.js
@@ -20,7 +20,7 @@ function responseData(result) {
             <td>${data.kategori}</td>
             <td>${data.waktu}</td>
             <td>${data.penulis}</td>
-            <td><a class="btn btn-primary btn-user btn-block" href="UpdateBerita.html?id=${data.id}&kategori=${data.kategori}&judul=${data.judul}&preview=${data.preview}&paragraf1=${data.paragraf1}&paragraf2=${data.paragraf2}&paragraf3=${data.paragraf3}&paragraf4=${data.paragraf4}&paragraf5=${data.paragraf5}&paragraf6=${data.paragraf6}&paragraf7=${data.paragraf7}&paragraf8=${data.paragraf8}&paragraf9=${data.paragraf9}&paragraf10=${data.paragraf10}&sumber=${data.sumber}&image=${data.image}">Update</a></td>
+            <td><a class="btn btn-primary btn-user btn-block" href="UpdateBerita.html?id=${data.id}&kategori=${data.kategori}&judul=${data.judul}&preview=${data.preview}&paragraf1=${data.konten.paragraf1}&paragraf2=${data.konten.paragraf2}&paragraf3=${data.konten.paragraf3}&paragraf4=${data.konten.paragraf4}&paragraf5=${data.konten.paragraf5}&paragraf6=${data.konten.paragraf6}&paragraf7=${data.konten.paragraf7}&paragraf8=${data.konten.paragraf8}&paragraf9=${data.konten.paragraf9}&paragraf10=${data.konten.paragraf10}&sumber=${data.sumber}&image=${data.image}">Update</a></td>
             <td><a class="btn btn-danger btn-user btn-block" href="DeleteBerita.html?id=${data.id}">Delete</a></td>
         `;
         // Append the post div to the post container
